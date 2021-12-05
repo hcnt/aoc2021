412f34911b6fe2c57613028f81c4cd84669fd86f
Jacek Kuśmierczyk <jacek.kusmierczyk1@gmail.com>
2021-12-05 11:50:21 +0100

    style fixes

--- day1/solve.py
+++ day1/solve.py
@@ -1,11 +1,7 @@
-def solve1(data):
-    return sum(data[i] < data[i + 1] for i in range(len(data) - 1))
-
-
-def solve2(data):
-    return sum(data[i] < data[i + 3] for i in range(len(data) - 3))
+def solve(data, jump):
+    return sum(data[i] < data[i + jump] for i in range(len(data) - jump))
 
 
 with open("input", "r") as f:
     data = [line.strip() for line in f]
-print(f"part 1: {solve1(data)}, part 2: {solve2(data)}")
+print(f"part 1: {solve(data, 1)}, part 2: {solve(data, 3)}")
